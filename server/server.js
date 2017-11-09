775ce93d559a8f856235650028e34dd231376ae7
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-11-09 16:09:44 -0500

    Update server with start server and close server functions

--- server/server.js
+++ server/server.js
@@ -4,14 +4,46 @@ const Task = require('./api/models/ares-models');
 const bodyParser = require('body-parser');
 const express = require('express');
 const {  DATABASE_URL, PORT } = require('./config');
-const app = express();
 const routes = require('./api/routes/ares-routes');
+const app = express();
+let server;
 
 routes(app);
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 
+const runServer = (port = PORT, database = DATABASE_URL) => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(database, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+};
 
-app.listen(PORT);
+const closeServer = () => {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+};
 
-console.log('server listening on: ' + PORT);
+module.exports = {
+  app,
+  runServer,
+  closeServer
+};

2f4603f46c23214ce864a541784044ed12f3caa4
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-11-09 16:09:03 -0500

    Update api routes

--- server/api/routes/ares-routes.js
+++ server/api/routes/ares-routes.js
@@ -2,11 +2,11 @@
 module.exports = (app) => {
   const ares = require('../controllers/ares-controllers');
 
-  app.route('/tracks')
+  app.route('/api/tracks')
     .get(ares.list_all_tracks)
     .post(ares.create_tracks);
 
-  app.route('/tracks/:tracksId')
+  app.route('/api/tracks/:tracksId')
      .get(ares.read_a_task)
      .put(ares.update_a_track)
      .delete(ares.delete_a_track);
